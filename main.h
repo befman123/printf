b3373ea139aa98443f6304d0abb8d70dcf8eba78
befman123 <befman123@gmail.com>
2023-11-14 18:29:48 +0300

    Added a main.h file.

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+int _print_char(char);
+int _print_string(char *);
+int _printf(const char *, ...);
+
+#endif
